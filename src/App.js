e21140a2332f409e9cff8ff237efee5e7c871c09
Murad Sarker <sarker24@gmail.com>
2021-02-23 09:44:00 +0100

    Modify the code

--- src/App.js
+++ src/App.js
@@ -6,56 +6,17 @@ import Loading from './Loading';
 
 class App extends Component{ 
 
-  constructor(props){
-    super(props);
-
-    this.state = {
-      users: [],
-      loading: false
-    };
-    this.HandleSubmit = this.HandleSubmit.bind(this);
-  }
-
-  getUsers(){
-    this.setState({
-      loading: true
-    })
-
-    axios('https://api.randomuser.me/?nat=US&results=5').then(respose =>
-  
-    this.setState({
-      users: respose.data.results,
-      loading: false
-    })
-    );  
-
-  }
-
-componentWillMount(){
- this.getUsers();
-}
-
-HandleSubmit(e) {
-  e.preventDefault();
-  this.getUsers();
-  console.log('More user loading')
-}
-
   render(){
-  return <div className="App">
-    {!this.state.loading ? this.state.users.map(user =>
+  return (
+    
        <div>
-         <p>
-         {user.name.first}
-         </p>
-         <form onSubmit= {this.HandleSubmit}>
-           <input type='submit' value='Load users' />
-         </form>
-
-      </div>): <Loading />}
+         <h3>This is for testing</h3>
     </div>
 
-} 
+
+);
+  }
 }
+
 
 export default App;
